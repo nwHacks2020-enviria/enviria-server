0eca15ca8c2bf5b6f33c60d50f7a75b9f3afd34b
Ibrahim Aly <ialy@ualberta.ca>
2020-01-12 12:46:25 -0800

    Final fixes

--- src/index.js
+++ src/index.js
@@ -14,11 +14,11 @@ var GreenScore = require('./greenscore');
 const app = express()
 
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/enviriadb', {useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost/enviriadb', { useNewUrlParser: true, useUnifiedTopology: true })
 var db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', function() {
-  console.log('Database Connection Established.')
+db.once('open', function () {
+    console.log('Database Connection Established.')
 })
 
 app.use(helmet())
@@ -30,13 +30,13 @@ app.use(cors())
 app.use(morgan('combined'))
 
 const addUpdateAuthToken = async (username, token) => {
-    await Token.findOne({username: username}, async function(err, oldToken){
-        if (oldToken){
+    await Token.findOne({ username: username }, async function (err, oldToken) {
+        if (oldToken) {
             oldToken.token = token
             oldToken.save()
             console.log('Token value updated: ', oldToken.token)
         } else {
-            var newToken = new Token({username, token, expiry: moment.utc().add(1, "M").toDate() })
+            var newToken = new Token({ username, token, expiry: moment.utc().add(1, "M").toDate() })
             var result = await newToken.save()
             console.log('Token added: ', result)
             console.log('Token value: ', newToken.token)
@@ -72,7 +72,7 @@ app.post("/authenticate", async (req, res) => {
     if (req.body.password == undefined) {
         res.send({ code: 500, message: "Please send password in body" })
     }
-    await User.findOne({username: req.body.username}, async function(err, user){
+    await User.findOne({ username: req.body.username }, async function (err, user) {
         if (user) {
             let hashed_password = user.password
             try {
@@ -97,7 +97,7 @@ app.post("/authenticate", async (req, res) => {
 })
 
 // Middleware
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     console.log("Secure resource " + Date.now())
 
     if (req.query.token == undefined) {
@@ -107,8 +107,8 @@ app.use(function(req, res, next){
         })
         return
     }
-    Token.findOne({token: req.query.token}, async function(err, token){
-        if (!token){
+    Token.findOne({ token: req.query.token }, async function (err, token) {
+        if (!token) {
             res.send({
                 code: 500,
                 message: "Token not detected"
@@ -117,12 +117,12 @@ app.use(function(req, res, next){
             next()
         }
     })
- })
+})
 
 app.post('/authenticateUsingToken', async (req, res) => {
     try {
-        await Token.findOne({token: req.query.token}, async function(err, token){
-            if (!token){
+        await Token.findOne({ token: req.query.token }, async function (err, token) {
+            if (!token) {
                 res.send({
                     code: 500,
                     message: "Token not valid, please login."
@@ -156,18 +156,26 @@ app.post('/authenticateUsingToken', async (req, res) => {
     }
 })
 
-async function getUserIDFromToken(token){
+async function getUserIDFromToken(token) {
     var username = null;
     var user_id = null;
     try {
-        await Token.findOne({token: token}, async function (err, db_token) {
+        await Token.findOne({ token: token }, async function (err, db_token) {
             if (db_token.username) {
                 username = db_token.username
+                console.log("BITCH")
             }
         })
-        await User.findOne({username: username}, function (err, user) {
-            user_id = user._id
-        })
+        console.log("!!!")
+        console.log("!!!")
+        console.log("!!!")
+        // console.log(await User.findOne({ username: username }))
+        // console.log(username)
+        console.log("!!!")
+        console.log("!!!")
+        console.log("!!!")
+        let result = await User.findOne({ username: username });
+        user_id = result._id
     } catch (err) {
         console.error(err)
     }
@@ -190,12 +198,12 @@ app.post('/api/greenscore', async function (req, res) {
     var totgreenscore = 0
 
     console.log(req.body.action)
-    if (actionScores[req.body.action] == undefined){
+    if (actionScores[req.body.action] == undefined) {
         res.send({ code: 500, message: "action not found" })
         return
     }
-    await User.findOne({_id: user_id}, async function(err, user){   
-        if(user){
+    await User.findOne({ _id: user_id }, async function (err, user) {
+        if (user) {
             user.total_greenscore += actionScores[req.body.action]
             if (user.total_greenscore > 10000)
                 user.total_greenscore = 10000;
@@ -203,15 +211,15 @@ app.post('/api/greenscore', async function (req, res) {
                 user.total_greenscore = 0;
 
             totgreenscore = user.total_greenscore
-            
+
             console.log(user.username + '\'s current greenscore:', user.total_greenscore)
             await user.save();
-        }else{
+        } else {
             console.log(err);
         }
     })
 
-    var greenscore = new GreenScore ({
+    var greenscore = new GreenScore({
         user_id: user_id,
         action: req.body.action,
         score: actionScores[req.body.action],
@@ -227,12 +235,12 @@ app.post('/api/greenscore', async function (req, res) {
     });
 })
 
-app.get('/api/greenscore', async function(req, res) {
+app.get('/api/greenscore', async function (req, res) {
     var token = req.query.token
     var user_id = await getUserIDFromToken(token)
     var result = []
 
-    const agg = GreenScore.aggregate([{ $match: { user_id: user_id}}])
+    const agg = GreenScore.aggregate([{ $match: { user_id: user_id } }])
     for await (const gs of agg) {
         result.push(gs)
     }
@@ -240,20 +248,20 @@ app.get('/api/greenscore', async function(req, res) {
         code: 200,
         data: result
     })
-})  
+})
 
-app.post('/api/friends', async function(req, res) {
+app.post('/api/friends', async function (req, res) {
     var token = req.query.token
     var friend = req.body.friend_username
     var friend_id = null
     var user_id = await getUserIDFromToken(token)
     var result = null
     try {
-        await User.findOne({username: friend}, async function (err, user){
+        await User.findOne({ username: friend }, async function (err, user) {
             friend_id = user._id
         })
-        await User.findOne({_id: user_id}, async function(err, user){
-            if (!friend_id || user_id.toString() == friend_id.toString()){
+        await User.findOne({ _id: user_id }, async function (err, user) {
+            if (!friend_id || user_id.toString() == friend_id.toString()) {
                 res.send({ code: 500, message: "Friend error" })
                 return
             }
@@ -275,17 +283,17 @@ app.get('/api/friends', async function (req, res) {
     var friends = null
     var result = []
     try {
-        await User.findOne({'_id': user_id}, function(err, user) {
+        await User.findOne({ '_id': user_id }, function (err, user) {
             if (user) {
                 friends = user.friends;
             }
         })
         await User.find({
-            '_id': { $in: friends}
-        }, function(err, users){
+            '_id': { $in: friends }
+        }, function (err, users) {
             users.forEach(function (item, index) {
-                let clean_user = {"username": item.username, "greenscore": item.total_greenscore}
-                result.push(clean_user) 
+                let clean_user = { "username": item.username, "greenscore": item.total_greenscore }
+                result.push(clean_user)
             })
         })
     } catch (error) {
@@ -307,12 +315,12 @@ app.delete('/api/friends', async function (req, res) {
     var user_id = await getUserIDFromToken(token)
     var friend_id = null
     try {
-        await User.findOne({'username': friend}, function(err, user) {
+        await User.findOne({ 'username': friend }, function (err, user) {
             if (user) {
                 friend_id = user._id
             }
         })
-        await User.findOne({'_id': user_id}, function(err, user) {
+        await User.findOne({ '_id': user_id }, function (err, user) {
             if (user) {
                 var index = user.friends.indexOf(friend_id)
                 if (index != -1) {
@@ -333,8 +341,8 @@ app.delete('/api/friends', async function (req, res) {
 app.get('/api/leaderboard', async function (req, res) {
     var top_users = []
     try {
-        User.find({}).sort({total_greenscore: 'desc'}).limit(10).exec(function(err, docs){
-            docs.forEach(function(result, index){
+        User.find({}).sort({ total_greenscore: 'desc' }).limit(10).exec(function (err, docs) {
+            docs.forEach(function (result, index) {
                 // console.log(result)
                 top_users.push({
                     username: result.username,
@@ -353,24 +361,28 @@ app.get('/api/leaderboard', async function (req, res) {
     } catch (error) {
         console.error(error)
     }
-    
+
 })
 
 app.post('/api/greenscoreAggregation', async function (req, res) {
     var token = req.query.token
     var user_id = await getUserIDFromToken(token)
-    var fromTime = new Date(req.body.fromTime)
-    var toTime = new Date(req.body.toTime)
+    try {
+        var fromTime = new Date(req.body.fromTime)
+        var toTime = new Date(req.body.toTime)
+    } catch (error) {
+        console.error(error)
+    }
 
     console.log(fromTime)
     console.log(toTime)
     var result = 0
 
     try {
-        const agg = GreenScore.aggregate([{ $match: { user_id: user_id}}])
+        const agg = GreenScore.aggregate([{ $match: { user_id: user_id } }])
         for await (const gs of agg) {
             if (gs.createdAt >= fromTime && gs.createdAt <= toTime) {
-                result += gs.score
+                result += actionScores[gs.action]
             }
         }
         console.log(result)
@@ -391,7 +403,7 @@ app.post('/api/greenscoreAggregation', async function (req, res) {
     }
 })
 
-app.post('/api/greenscoreByDay', async function(req, res) {
+app.post('/api/greenscoreByDay', async function (req, res) {
     var token = req.query.token
     var user_id = await getUserIDFromToken(token)
     var fromTime = new Date(req.body.fromTime)
@@ -402,7 +414,7 @@ app.post('/api/greenscoreByDay', async function(req, res) {
     var result = []
 
     try {
-        const agg = GreenScore.aggregate([{ $match: { user_id: user_id}}])
+        const agg = GreenScore.aggregate([{ $match: { user_id: user_id } }])
         for await (const gs of agg) {
             if (gs.createdAt >= fromTime && gs.createdAt <= toTime) {
                 result.push(gs)
